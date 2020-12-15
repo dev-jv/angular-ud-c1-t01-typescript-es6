e7e908fd10aafbe98a881d3820884fe2845510b7
dev-jv <dev.javierleon@gmail.com>
2020-12-15 17:28:34 -0500

    five

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
 "use strict";
 (function () {
-    let mensaje = 'Although';
-    let numero = 123;
-    let boolean = true;
-    let hoy = new Date();
-    // let any: string | number | Date | boolean;
-    let any;
-    any = mensaje;
-    any = numero;
-    any = boolean;
-    any = hoy;
-    let grimm = {
-        nombre: 'Nick',
-        edad: 30
-    };
-    grimm = {
-        nombre: 'troble',
-        edad: 28,
-    };
 })();
--- app.ts
+++ app.ts
@@ -1,26 +1,5 @@
 (function () {
-    let mensaje: string = 'Although';
-    let numero: number = 123;
-    let boolean: boolean = true;
-    let hoy: Date = new Date();
 
-    // let any: string | number | Date | boolean;
-    let any;
-    any = mensaje;
-    any = numero;
-    any = boolean;
-    any = hoy;
-
-    let grimm = {
-        nombre: 'Nick',
-        edad:30
-    };
-
-    grimm = {
-        nombre: 'troble',
-        edad: 28,
-        // estado: 'activo'
-    }
 })();
 
 
