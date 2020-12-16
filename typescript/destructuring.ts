3ad14ddd54f2e605088c111d4821cf7d24d61b9d
dev-jv <dev.javierleon@gmail.com>
2020-12-16 10:08:00 -0500

    nine

---
+++ typescript/destructuring.ts
@@ -0,0 +1,33 @@
+(() => {
+    const hunter = {
+        name: 'Nick Burkhardt',
+        blood: 'Grimm',
+        state: 'active'
+    };
+
+    const extraer = ({name, state }:any) => {
+        // const { name, state } = hunter;
+        console.log( name );
+        console.log( state );
+    };
+    extraer( hunter );
+
+    const wesen = ['Siegbarste', 'Hexenbiest', 'Fuchsbau'];
+
+    const [x, y, z] = wesen;
+
+    console.log(x);
+    console.log(y);
+    console.log(z);
+
+    const extraerArr = ([a,b,c]:String[]) => {
+        console.log(a);
+        console.log(b);
+        console.log(c);
+    };
+
+    extraerArr( wesen );
+})();
+
+
+
