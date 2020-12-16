55cc80f57813d8cd11b6bb9d26290a3abfc724ac
dev-jv <dev.javierleon@gmail.com>
2020-12-16 15:49:20 -0500

    eleven

---
+++ typescript/promise-typescript.ts
@@ -0,0 +1,21 @@
+(() => {
+    const retirarDinero = (montoRetirar:number):Promise<number> => {
+        let dineroActual = 1000;
+        console.log('maybe');
+        return new Promise((resolve, reject) => {
+            if(montoRetirar>dineroActual){
+                reject('No hay suficientes fondos');
+            }else{
+                dineroActual -= montoRetirar;
+                resolve(dineroActual);
+            }
+        })
+    };
+
+    retirarDinero( 500 )
+        .then( montoActual => console.log(`Me queda ${ montoActual }`))
+        .catch( console.warn )
+})();
+
+
+
